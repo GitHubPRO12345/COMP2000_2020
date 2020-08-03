c1789a69861e0bbaa6c219d64db99157c766d3b2
GitHubPRO12345 <53499627+GitHubPRO12345@users.noreply.github.com>
2020-08-04 09:42:20 +1000

    Drew Grid

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
+        //window.run();
     }
+
+    public class Canvas extends JPanel {
+        public Canvas(){
+            setPreferredSize(new Dimension(720,720));
+        }
+
+        @Override
+        public void paint(Graphics g){
+            for(int i =10; i<710; i+=35){
+                for(int j=10; j<710; j+=35){
+                    g.setColor(Color.WHITE);
+                    g.fillRect(j, 10, 35, 35);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i, j, 35, 35);
+                }
+            }
+        }
+    }
+
+    public Main(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);  
+    }
+
+    public void grid(){
+        
+    }
+
+    public void cell(){
+
+    }
+
+    
 }
